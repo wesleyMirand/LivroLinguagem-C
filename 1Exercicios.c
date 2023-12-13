d8139beb476ba75e7ba695fb375f0a8bab96c5b5
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-13 11:55:14 -0300

    Create 1Exercicios.c

---
+++ 1Exercicios.c
@@ -0,0 +1,21 @@
+//pagina 90 - Ler 10 elementos de uma matriz vetor
+#include <stdio.h>
+
+int v[10];
+
+int main()
+{
+  v [0] = 45;//armazenando valor na var
+  V [1] = 78;//indice 1 valor 78
+  V [2] = 9;
+  V [3] = 5;
+  V [4] = 5;
+  V [5] = 5;
+  V [6] = 5;
+  V [7] = 5;
+  V [8] = 5;
+  V [9] = 5;
+
+  
+  return 0;
+}
