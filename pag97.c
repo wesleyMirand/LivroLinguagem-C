3067e3593ba446e72803386b76953646d9839fea
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-18 22:12:46 -0300

    Create pag97.c

---
+++ pag97.c
@@ -0,0 +1,10 @@
+//pede dados pessoas com gets
+
+main() 
+{
+char nome[20], sb_nome[20];
+printf("digite o nome:.........."); gets(nome);
+printf("digite o sobre nome:.........."); gets("sb_nome);
+
+printf("Seja bem vindo, %s %s", nome, sb_nome);
+}
