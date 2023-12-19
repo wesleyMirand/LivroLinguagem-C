00afc465e076b6282234306b42cb1bac8c723e4b
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-18 22:11:34 -0300

    Create pag96.c

---
+++ pag96.c
@@ -0,0 +1,10 @@
+//pede dados pessoas
+
+main() 
+{
+char nome[10], sb_nome[15];
+printf("digite o nome:.........."); scanf("%s", nome);
+printf("digite o sobre nome:.........."); scanf("%s", sb_nome);
+
+printf("Seja bem vindo, %s %s", nome, sb_nome);
+}
